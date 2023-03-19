d505d89ab167906527ed03c497a5eb1d405d1e59
Korney Saushkin <saukorn03@gmail.com>
2023-03-19 19:31:15 +0100

    Add class Home, Homegetter and Homesetter

---
+++ src/Home.java
@@ -0,0 +1,16 @@
+public abstract class Home {
+
+  private Home adress;
+
+  public Home(Home adress) {
+    this.adress = adress;
+  }
+}
+
+  public setHome(Home adress) {
+    adress = adress;
+  }
+
+  public getHome() {
+    return.adress;
+  }
